a63a1ab8f12cfdc500327eea51c18a06498e7b0a
Lukas Bellaart <99068241@mydavinci.nl>
2021-09-06 11:59:55 +0200

    python programmas

--- feestlunch.py
+++ feestlunch.py
@@ -5,5 +5,5 @@ stokbroodWaarde = 2.78
 kortingBonnen = 3
 kortingWaarde = 0.50
 
-
-print(crossaints * cossaintsPrijs + stokbrood * stokbroodWaarde - kortingBonnen * kortingWaarde )
+koste = crossaints * cossaintsPrijs + stokbrood * stokbroodWaarde - kortingBonnen * kortingWaarde
+print('De feestlunch kost je bij de bakker ' + str(koste) + ' euro voor de ' + str(crossaints) + ' croissantjes en de '+ str(stokbrood)+' stokbroden als de '+str(kortingBonnen)+' kortingsbonnen nog geldig zijn')
--- speelhaldag.py
+++ speelhaldag.py
@@ -4,4 +4,7 @@ personen = 3
 minuten = 45
 
 
-print(minuten / 5 * perVijfMinPP * personen)
+koste = minuten / 5 * perVijfMinPP * personen
+
+#print('De feestlunch kost je bij de bakker ' + str(koste) + ' euro voor de ' + str(crossaints) + ' croissantjes en de '+ str(stokbrood)+' stokbroden als de '+str(kortingBonnen)+' kortingsbonnen nog geldig zijn')
+print('Dit geweldige dagje-uit met ' + str(personen) + ' mensen in de Speelhal met ' + str(minuten) + ' minuten VR kost je maar ' + str(koste) + ' euro')
