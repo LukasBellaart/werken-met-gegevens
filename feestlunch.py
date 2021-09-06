b8f66fad7e3b902d366048eaaffd2949511ac082
Lukas Bellaart <99068241@mydavinci.nl>
2021-09-06 11:53:30 +0200

    python programmas

---
+++ feestlunch.py
@@ -0,0 +1,9 @@
+crossaints = 17
+cossaintsPrijs = 0.39
+stokbrood = 2
+stokbroodWaarde = 2.78
+kortingBonnen = 3
+kortingWaarde = 0.50
+
+
+print(crossaints * cossaintsPrijs + stokbrood * stokbroodWaarde - kortingBonnen * kortingWaarde )
---
+++ speelhaldag.py
@@ -0,0 +1,7 @@
+perVijfMinPP = 0.37
+
+personen = 3
+minuten = 45
+
+
+print(minuten / 5 * perVijfMinPP * personen)
